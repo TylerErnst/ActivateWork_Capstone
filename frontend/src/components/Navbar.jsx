58ffd777aa54a1d5b88c33bc038e5a382d62701b
TylerErnst <tcernst5@gmail.com>
2024-06-13 08:54:00 -0600

    fixed a logout error

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,9 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { signOut } from 'firebase/auth';
 import { auth } from '../firebase/config';
-import { useNavigate } from "react-router-dom";
-
 
 function Navbar({ user, setUser }) {
-    const navigate = useNavigate()
+    const navigate = useNavigate();
 
     const logOut = async () => {
         try {
@@ -17,31 +15,31 @@ function Navbar({ user, setUser }) {
         }
     };
 
-  return (
-    <nav className="nav">
-        <Link to="/" className="site-title">Home</Link>
-        <ul>
-            <li>
-                <Link to="/search/1">Add Items</Link>
-            </li>
-            <li>
-                <Link to="/netwealth/1">Net Wealth</Link>
-            </li>
-            <li>
-                <Link to="/about">About</Link>
-            </li>
-            {!user.user && <li>
-                <Link to="/signup">SignUp</Link>
-            </li>}
-            {!user.user && <li>
-                <Link to="/login">Login</Link>
-            </li>}
-            {user.user && <li>
-                <span onClick={logOut}>Logout</span>
-            </li>}
-        </ul>
-    </nav>
-  );
+    return (
+        <nav className="nav">
+            <Link to="/" className="site-title">Home</Link>
+            <ul>
+                <li>
+                    <Link to="/search/1">Add Items</Link>
+                </li>
+                <li>
+                    <Link to="/netwealth/1">Net Wealth</Link>
+                </li>
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+                {!user?.user && <li>
+                    <Link to="/signup">SignUp</Link>
+                </li>}
+                {!user?.user && <li>
+                    <Link to="/login">Login</Link>
+                </li>}
+                {user?.user && <li>
+                    <span onClick={logOut}>Logout</span>
+                </li>}
+            </ul>
+        </nav>
+    );
 }
 
 export default Navbar;
